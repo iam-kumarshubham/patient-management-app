44b88847684b11c26c4b7f16df4fc5ac0647451b
KUMAR SHUBHAM <56132490+iam-kumarshubham@users.noreply.github.com>
2025-05-07 11:17:40 +0530

    feat: Add basic component and service structure

---
+++ src/app/components/sql-query/sql-query.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sql-query',
+  imports: [],
+  templateUrl: './sql-query.component.html',
+  styleUrl: './sql-query.component.scss'
+})
+export class SqlQueryComponent {
+
+}
